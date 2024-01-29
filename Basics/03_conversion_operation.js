c27214165ed416df18f076510615aef5ff3a2a25
astrondiya <158036991+astrondiya@users.noreply.github.com>
2024-01-29 11:21:41 +0000

    operation done

--- Basics/03_conversion_operation.js
+++ Basics/03_conversion_operation.js
@@ -10,8 +10,29 @@ console.log(valueInNumber); //NaN - not a number
 // TRUE => 1; FALSE => 0
 
 let isLoggedIn = "true"
-let booleanIsLoggedIn = Boolean(isLoogedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
 
 // null => 0
 // "diya" => true
+
+
+// ***************** Operations ***************
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2)
+
+let str1 = "Hello"
+let str2 = "Diya"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);  //12
+console.log(1 + "2");  //12
+console.log(1 + 1 + "2");  //22
+console.log("1" + "1" +2); //112
+
+// prefix and postfix operator
