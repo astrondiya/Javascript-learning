7476ef6063d7bc2f4247aec85474e98e941b7c95
astrondiya <158036991+astrondiya@users.noreply.github.com>
2024-01-29 10:30:25 +0000

    variables done

---
+++ Basics/01_variables.js
@@ -0,0 +1,15 @@
+//1
+const accountId = 144625
+// const can't be reassigned nor it can be redeclared
+
+//2
+let accountEmail = "dk144@gmail.com"
+// variables can be declared in two ways - var (issue in block scope and functional scope) and let
+
+//3
+var accountPassword = "12345"
+
+//4
+accountCity = "Jaipur"
+
+// console.table([var1, var2,..]) --> table of variables
