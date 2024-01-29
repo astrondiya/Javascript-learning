5c0d809ad4c5f4075734c0d61c5ac1a105e1f5ca
astrondiya <158036991+astrondiya@users.noreply.github.com>
2024-01-29 10:31:16 +0000

    data types done

---
+++ Basics/02_data_types.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using node.js, not browser
+
+//string
+let name = "hitesh"
+
+// number -> 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value [type is object]
+// undefined => not assigned any value
+// symbol => unique
+
+// object
+
