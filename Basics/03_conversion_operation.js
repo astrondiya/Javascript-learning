69b98e380ddb0d9280d59afc9de8589765c4d82f
astrondiya <158036991+astrondiya@users.noreply.github.com>
2024-01-29 11:00:11 +0000

    conversion of data types

---
+++ Basics/03_conversion_operation.js
@@ -0,0 +1,17 @@
+let score = "33abc"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber); //NaN - not a number
+// 33 => 33
+// "33abc" => NaN
+// TRUE => 1; FALSE => 0
+
+let isLoggedIn = "true"
+let booleanIsLoggedIn = Boolean(isLoogedIn)
+console.log(booleanIsLoggedIn);
+
+// null => 0
+// "diya" => true
