78126f799ec1c4679916c6d12a7f8e9968772e93
astrondiya <158036991+astrondiya@users.noreply.github.com>
2024-01-29 11:31:28 +0000

    comparisons done

---
+++ Basics/04_comparisons.js
@@ -0,0 +1,16 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(1==1);
+
+// compare only two same datatypes
+
+/* Avoid this syntax */
+//Equality check and comparisons work differently; comparisons converts null to 0
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// === 
+console.log("2"===2);
