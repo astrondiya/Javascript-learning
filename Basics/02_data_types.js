8bd109b3052364c7a283a35fe1a9cbe641b8193b
astrondiya <158036991+astrondiya@users.noreply.github.com>
2024-01-30 06:38:21 +0000

    Modified

--- Basics/02_data_types.js
+++ Basics/02_data_types.js
@@ -14,4 +14,30 @@ let name = "hitesh"
 // symbol => unique
 
 // object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
 
+
+// Two categorisation based on how the data is stored in memory and how it is accesssed
+// primitive and non-primitive data types
+
+// Loosely-typed language
+
+
+// 7P : String, Number, Boolean, null, undefined, Symbol, Bigint
+
+
+// Reference (non-primitive) : Array, Objects, Functions
+//1
+const fruits = ["apple", "mango", "pineapple"];
+
+//2
+let myObj = {
+    name: "Diya",
+    age: 22,
+}
+
+//3 
+const myFunction = function(){
+    console.log("Hello, world!");
+}
--- Basics/04_comparisons.js
+++ Basics/04_comparisons.js
@@ -2,7 +2,7 @@ console.log(2>1);
 console.log(2>=1);
 console.log(1==1);
 
-// compare only two same datatypes
+// compare only same datatypes
 
 /* Avoid this syntax */
 //Equality check and comparisons work differently; comparisons converts null to 0
@@ -12,5 +12,5 @@ console.log(undefined > 0);
 console.log(undefined < 0);
 
 
-// === 
-console.log("2"===2);
+// === (strict checking i.e. apart from value it also compares the data type)
+console.log("2"===2); 
