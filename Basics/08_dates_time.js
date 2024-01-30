42b6428863d9652743f3ca63ee0f33dfc30214cd
astrondiya <158036991+astrondiya@users.noreply.github.com>
2024-01-30 11:16:12 +0000

    date and time added

---
+++ Basics/08_dates_time.js
@@ -0,0 +1,24 @@
+// Dates
+
+const myDate = new Date()
+console.log(myDate);
+console.log(typeof myDate); //object
+
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString('en-IN'));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
